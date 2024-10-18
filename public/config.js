e0a760c291a2c6cc716db4145975c62c2abe2be8
Gage <gagedorlando@gmail.com>
2024-10-17 19:49:33 -0600

    Created env file

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC4b6iuMlqXE8Sv06lunKYFvhxIyHQBZ70",
+    apiKey: process.env.apiKey,
     authDomain: "gage-d-orlando-portfolio.firebaseapp.com",
     projectId: "gage-d-orlando-portfolio",
     storageBucket: "gage-d-orlando-portfolio.appspot.com",
-    messagingSenderId: "550423788733",
-    appId: "1:550423788733:web:7ff6e12a6efc7ee16bcbd4",
-    measurementId: "G-GCWLMP44QB"
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
