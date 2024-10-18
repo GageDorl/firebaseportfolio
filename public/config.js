b442e01a85db500f55b58a67f3dfce6ee1759b08
Gage <gagedorlando@gmail.com>
2024-10-17 19:44:47 -0600

    Hopefully this pushed right

---
+++ public/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC4b6iuMlqXE8Sv06lunKYFvhxIyHQBZ70",
+    authDomain: "gage-d-orlando-portfolio.firebaseapp.com",
+    projectId: "gage-d-orlando-portfolio",
+    storageBucket: "gage-d-orlando-portfolio.appspot.com",
+    messagingSenderId: "550423788733",
+    appId: "1:550423788733:web:7ff6e12a6efc7ee16bcbd4",
+    measurementId: "G-GCWLMP44QB"
+  };
