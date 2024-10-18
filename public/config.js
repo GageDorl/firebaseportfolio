6852052d3b3f8d8e50f151e3c56688584f7f2073
Gage <gagedorlando@gmail.com>
2024-10-17 20:40:02 -0600

    Set env variables

---
+++ public/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: "gage-d-orlando-portfolio.firebaseapp.com",
+  projectId: "gage-d-orlando-portfolio",
+  storageBucket: "gage-d-orlando-portfolio.appspot.com",
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+  
